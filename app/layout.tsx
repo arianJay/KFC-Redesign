09a97a281441c63f02211aef5cefa066df8996ad
arianJay <arianjay.prisco@carsu.edu.ph>
2025-12-08 17:35:51 +0800

    fixed darkmode to lightmode default part 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`font-sans antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <CartProvider>
             <OrderProvider>
               {children}
