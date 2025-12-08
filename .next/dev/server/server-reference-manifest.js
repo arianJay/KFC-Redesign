c3caccf56038794ae1fd34507928e3dff4067bb9
arianJay <arianjay.prisco@carsu.edu.ph>
2025-12-08 17:02:30 +0800

    Remove .next build cache from Git tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jQ/HvvWwwFUcQ/v1BygJAxD6l7QApuPRV5R5YeUyKEY=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_0b377259._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_53d39439._.js",
-  "static/chunks/_bd7f8e90._.js"
-],
-    source: "dynamic"
-});
